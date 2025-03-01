9f0984fa92fd26fc42166875f7cbe4102eb05ab8
chan <curiouscomponent@gmail.com>
2025-03-01 22:32:29 +0900

    setup: SpringBoot 기본 구조 설정
    Dependencies 설정

---
+++ src/test/java/net/cproduction/booknotification/BookNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cproduction.booknotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
