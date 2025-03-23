76c38e8d63fa368191d3a4ae3d16a250dd0c1ea4
chan <curiouscomponent@gmail.com>
2025-03-23 11:44:09 +0900

    test: test 관련 프로퍼티 수정
    - 테스트 시 test profile적용
    - 더미데이터 프로퍼티 생성
    - workflow env값 삭제

--- src/test/java/net/cproduction/booknotification/BookNotificationApplicationTests.java
+++ src/test/java/net/cproduction/booknotification/BookNotificationApplicationTests.java
@@ -2,7 +2,9 @@ package net.cproduction.booknotification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BookNotificationApplicationTests extends AbstractIntegrationTest {
 
